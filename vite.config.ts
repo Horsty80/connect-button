dae22d772e586511cd7fef0d322704baee33f95a
Cyril Hue <cy.hue80@gmail.com>
2022-03-05 01:23:03 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "happy-dom",
+  },
+  build: {
+    lib: {
+      entry: "src/my-element.ts",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: /^lit/,
+    },
+  },
+});
